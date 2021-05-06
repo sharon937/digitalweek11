5404140a2c2173e35f937576cdc51cb786792aed
陳品璇 <83741454+sharon937@users.noreply.github.com>
2021-05-06 11:45:08 +0800

    Add files via upload

---
+++ wk11_02/wk11_02.ino
@@ -0,0 +1,26 @@
+  const int buttonPin=2;
+  const int ledPin=13;
+
+  int buttonState=0;
+  
+  void setup() {
+  // put your setup code here, to run once:
+   Serial.begin(9600);
+   pinMode(ledPin,OUTPUT);
+   pinMode(buttonPin,INPUT_PULLUP);
+  
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+buttonState=digitalRead(buttonPin);
+if(buttonState==HIGH){
+  digitalWrite(ledPin,HIGH);
+}
+else{
+  digitalWrite(ledPin,LOW);
+}
+Serial.println(buttonState);
+delay(1);
+}
